906939ae8cbf86d699e3507a3441d058a05d32a7
dodoki <dodo.ki.fone@gmail.com>
2022-05-05 18:44:44 +0200

    co ti drbe ono to funguje

--- main.py
+++ main.py
@@ -14,8 +14,8 @@ MOTOR_LAVY = Motor(Port.B, positive_direction=Direction.COUNTERCLOCKWISE, gears=
 MOTOR_PRAVY = Motor(Port.C,  positive_direction=Direction.COUNTERCLOCKWISE, gears=[8, 24])
 pohyb = DriveBase(MOTOR_PRAVY, MOTOR_LAVY, wheel_diameter = 45, axle_track = 50)
 #male motory
-MOTOR_MALY_PRAVY = Motor(Port.D)
-MOTOR_MALY_LAVY = Motor(Port.A)
+MOTOR_MALY_PRAVY = Motor(Port.D, positive_direction=Direction.COUNTERCLOCKWISE, gears=[1, 1])
+MOTOR_MALY_LAVY = Motor(Port.A, positive_direction=Direction.COUNTERCLOCKWISE, gears=[1, 1])
 
 #senzory
 PREDNY_SENZOR =  InfraredSensor(Port.S3)
@@ -32,39 +32,30 @@ MAX_SPEED = 100
 MID_SPEED = 75
 LOW_SPEED = 50
 
-def radlica_closed():
-    MOTOR_MALY_LAVY.run_target(LOW_SPEED, 5, then=Stop.HOLD, wait=False)
-    MOTOR_MALY_PRAVY.run_target(LOW_SPEED, -5, then=Stop.HOLD, wait=False)
-
-def radlica_opened():
-    MOTOR_MALY_LAVY.run_target(LOW_SPEED, -5, then=Stop.HOLD, wait=False)
-    MOTOR_MALY_PRAVY.run_target(LOW_SPEED, 5, then=Stop.HOLD, wait=False)
-
-
 
 def start(): #start ako S
     global opakovanie
-while opakovanie < 2:
-    pohyb.drive(LOW_SPEED, 0)
+    while opakovanie < 2:
+        pohyb.drive(LOW_SPEED, 0)
 
-    while PREDNY_SENZOR.distance() > 40:
-        wait(10)
+        while PREDNY_SENZOR.distance() > 40:
+            wait(10)
 
-    pohyb.stop()
-    wait(500)
-    pohyb.turn(55)
-    opakovanie += 1
+        pohyb.stop()
+        wait(500)
+        pohyb.turn(55)
+        opakovanie += 1
 
-while opakovanie < 4:
-    pohyb.drive(LOW_SPEED, 0)
+    while opakovanie < 4:
+        pohyb.drive(LOW_SPEED, 0)
 
-    while PREDNY_SENZOR.distance() > 40:
-        wait(10)
+        while PREDNY_SENZOR.distance() > 40:
+            wait(10)
 
-    pohyb.stop()
-    wait(500)
-    pohyb.turn(-55)
-    opakovanie += 1
+        pohyb.stop()
+        wait(500)
+        pohyb.turn(-55)
+        opakovanie += 1
 
 def stena():
     pohyb.turn(10)
@@ -81,27 +72,45 @@ def stena():
 def napravenie():
     pohyb.straight(-50)
     pohyb.turn(-55)
-    pohyb.straight(-10)
+    pohyb.straight(-15)
+    pohyb.straight(50)
 
 def hladanie():
-    pohyb.straight(10)
-    pohyb.turn(5)
-    if PREDNY_SENZOR != 30:
-        pohyb.drive(LOW_SPEED, -100)
-    else:
-        pohyb.stop()
+    global opakovanie
+    opakovanie = 0
+    radlica_opened()
+    pohyb.turn(30)
+    while opakovanie <= 1:
+        if PREDNY_SENZOR.distance() > 40 :
+            pohyb.drive(0.5, -100)
+        elif PREDNY_SENZOR.distance() <= 40:
+            pohyb.drive(LOW_SPEED, 0)
+            if PREDNY_SENZOR.distance() <= 10 :
+                radlica_closed()
+                opakovanie += 1
+            else:
+                print(PREDNY_SENZOR.distance())
+        else:
+            print("v riti")       
+     
     
+    
+def radlica_closed():
+    MOTOR_MALY_LAVY.run_target(1000, -80, then=Stop.HOLD, wait=False)
+    MOTOR_MALY_PRAVY.run_target(1000, 80, then=Stop.HOLD, wait=False)
 
+def radlica_opened():
+    MOTOR_MALY_LAVY.run_target(1000, 80, then=Stop.HOLD, wait=False)
+    MOTOR_MALY_PRAVY.run_target(1000, -80, then=Stop.HOLD, wait=False)
 
 
 def main():
-    #start()
-    #stena()
-    #napravenie()
-    #hladanie()
-    radlica_opened()
-    wait(5000)
-    radlica_closed()
+    start()
+    stena()
+    napravenie()
+    wait(1000)
+    hladanie()
+    wait(1000)
 
 if __name__ == "__main__":
     main()
